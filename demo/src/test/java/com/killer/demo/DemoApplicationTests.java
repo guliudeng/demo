dd6d3c8ef5700452e0d3530306880f5c686207aa
gld <2590107513@qq.com>
2022-06-20 18:33:28 +0800

    枚举使用

--- demo/src/test/java/com/killer/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/killer/demo/DemoApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
+import java.util.Arrays;
+
 @SpringBootTest
 class DemoApplicationTests {
 
@@ -18,11 +20,22 @@ class DemoApplicationTests {
      */
     @Test
     void test1() {
-        String pp = "P";
-        System.out.println(TaskType.P);
-        boolean b = TaskType.P.equals(TaskType.P);
-        System.out.println(b);
+        //获取枚举中所有的值
+        TaskType[] values = TaskType.values();
+        System.out.println(Arrays.asList(values));
+        //可以直接获取枚举本身
+        TaskType green = TaskType.GREEN;
+        System.out.println(green);
+        //也可以给枚举设置参数，然后提供公共构造方法来获取枚举对象身上的参数，
+        String red = TaskType.getName("red");
+        System.out.println(red);
+        boolean red1 = "红色".equals(red);
+        System.out.println(red1);
 
+        String gg = TaskType.getValud("绿色");
+        System.out.println(gg);
+        boolean green1 = gg.equals("green");
+        System.out.println(green1);
     }
 
     /**
